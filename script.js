bf830290c7c04c555332b264d372ca1aec6067d3
Matheus França <mateusanches2@gmail.com>
2021-07-13 23:13:23 -0300

    Modal

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const queryAll = (element) => { return document.querySelectorAll(element); }
 pizzaJson.map((item, index) => {
   let pizzaItem = query('.models .pizza-item').cloneNode(true);
 
-  
   pizzaItem.querySelector('.pizza-item--img img' ).setAttribute('src', item.img);
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.querySelector('a').addEventListener('click', (event) => {
+    event.preventDefault();
+    const modal = query('.pizzaWindowArea');
+    modal.classList.add('active');
+  })
 
   const pizzaArea = query('.pizza-area').append(pizzaItem);
+  return pizzaItem;
 });
