eb248cbd3e294f37a06bff0c52b94298e20c02e3
Matheus França <mateusanches2@gmail.com>
2021-07-14 19:56:18 -0300

    Add quantity and prices.

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-const query = (element) => { return document.querySelector(element); }
-const queryAll = (element) => { return document.querySelectorAll(element); }
+const query = (element) => {
+  return document.querySelector(element);
+}
+const queryAll = (element) => {
+  return document.querySelectorAll(element);
+}
+
+let quantity = 1;
+
 
 pizzaJson.map((item, index) => {
-  let pizzaIndex = index;
   let pizzaItem = query('.models .pizza-item').cloneNode(true);
 
-  pizzaItem.querySelector('.pizza-item--img img' ).setAttribute('src', item.img);
+  pizzaItem.querySelector('.pizza-item--img img').setAttribute('src', item.img);
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
   pizzaItem.querySelector('a').addEventListener('click', (event) => {
     event.preventDefault();
+    quantity = 1;
+
     const modal = query('.pizzaWindowArea');
-    const nome = query('.pizzaInfo h1');
+    const pizzaName = query('.pizzaInfo h1');
     const pizzaDescription = query('.pizzaInfo--desc');
-    const pizzaImg = query('.pizzaBig img'); 
-    
+    const pizzaImg = query('.pizzaBig img');
+    const pizzaPrice = query('.pizzaInfo--actualPrice');
+    const pizzaSize = queryAll('.pizzaInfo--size');
+    const pizzaSelected = query('.pizzaInfo--size.selected');
+    const pizzaQuantity = query('.pizzaInfo--qt');
+
     modal.classList.add('active');
-    nome.innerHTML = pizzaJson[pizzaIndex].name;
-    pizzaDescription.innerHTML = pizzaJson[pizzaIndex].description;
-    pizzaImg.setAttribute('src', pizzaJson[pizzaIndex].img);
+    pizzaName.innerHTML = item.name;
+    pizzaDescription.innerHTML = item.description;
+    pizzaImg.setAttribute('src', item.img);
+    pizzaPrice.innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaSelected.classList.remove('selected');
+    pizzaSize.forEach((size, sizeIndex) => {
+      if (sizeIndex === 2) {
+        size.classList.add('selected');
+      }
+      size.querySelector('span').innerHTML = pizzaJson[index].sizes[sizeIndex];
+    });
+    pizzaQuantity.innerHTML = quantity;
+
   });
 
   const pizzaArea = query('.pizza-area').append(pizzaItem);
