ac38a8f7673eef1e7fff30847024f58e2006deaa
Matheus França <mateusanches2@gmail.com>
2021-07-13 22:45:36 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const query = (element) => { return document.querySelector(element); }
+const queryAll = (element) => { return document.querySelectorAll(element); }
+
+pizzaJson.map((item, index) => {
+  let pizzaItem = query('.models .pizza-item').cloneNode(true);
+
+  
+  pizzaItem.querySelector('.pizza-item--img img' ).setAttribute('src', item.img);
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  const pizzaArea = query('.pizza-area').append(pizzaItem);
+});
